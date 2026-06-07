005206f8976122d2a9f80ff6eb6225131765a065
achyut66 <green.band66@gmail.com>
2026-06-07 10:34:18 +0545

    google analytics is added

---
+++ react/src/analytics.js
@@ -0,0 +1,12 @@
+import ReactGA from "react-ga4";
+
+export const initGA = () => {
+  ReactGA.initialize("G-GS04HTCC82");
+};
+
+export const logPageView = () => {
+  ReactGA.send({
+    hitType: "pageview",
+    page: window.location.pathname,
+  });
+};
